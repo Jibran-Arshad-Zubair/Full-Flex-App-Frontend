1298431644f208c214471cd58d81214f96afcbfb
Jibran Arshad <ranajibranarshad@gmail.com>
2025-08-03 17:06:35 +0500

    Use the react icons insted of the coustom svgs

--- src/components/chatComponents/MessageInput.jsx
+++ src/components/chatComponents/MessageInput.jsx
@@ -1,11 +1,11 @@
-import { PaperAirplaneIcon, FaceSmileIcon } from '@heroicons/react/24/solid';
+import { FaPaperPlane, FaSmile } from 'react-icons/fa';
 
 const MessageInput = () => {
   return (
     <div className="p-4 border-t border-gray-200 bg-white">
       <div className="flex items-center gap-2">
         <button className="text-gray-500 hover:text-blue-500">
-          <FaceSmileIcon className="h-6 w-6" />
+          <FaSmile className="h-6 w-6" />
         </button>
         <input
           type="text"
@@ -13,7 +13,7 @@ const MessageInput = () => {
           className="flex-1 px-4 py-2 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
         />
         <button className="p-2 rounded-full bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
-          <PaperAirplaneIcon className="h-5 w-5 rotate-270" />
+          <FaPaperPlane className="h-5 w-5 " />
         </button>
       </div>
     </div>
--- src/components/dashboard/Navbar.jsx
+++ src/components/dashboard/Navbar.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import UserDropdown from './UserDropdown';
 
 const Navbar = ({ toggleSidebar }) => {
